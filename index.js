2d4d6efeb0a37b2b75ed39bbf6afdf06410f6e4b
Jed Peek <jedpeek@yahoo.com>
2018-08-31 20:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function scuberGreetingForFeet(feet){
 };
 
 function ternaryCheckCity(city){
-  if city == "NYC" ? "Ok, sounds good." : "No go."
+  return city == "NYC" ? "Ok, sounds good." : "No go."
 }
